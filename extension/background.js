e82120520892725cb301e27938e03dcdf50595da
Nik Edmiidz <edmiidz@gmail.com>
2024-01-02 17:00:16 -0800

    Adding autowikica

--- extension/background.js
+++ extension/background.js
@@ -1,6 +1,6 @@
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
     // Check if the URL of the tab matches either the old or new domain
-    if (tab.url && (tab.url.includes('olddomain.com') || tab.url.includes('newdomain.com'))) {
+    if (tab.url && (tab.url.includes('autowikica.sharepoint.com/sites/TestPlan') || tab.url.includes('autowikica.sharepoint.com/sites/IntranetSample'))) {
       // Maintain or set the icon to the default (for matched domains)
       chrome.browserAction.setIcon({path: 'images/logo.png', tabId: tabId});
     } else {
--- extension/popup.js
+++ extension/popup.js
@@ -2,12 +2,12 @@ document.addEventListener('DOMContentLoaded', function() {
   var link = document.getElementById('mirrorLink');
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
     var currentTab = tabs[0];
-    if (currentTab.url.includes('olddomain.com')) {
+    if (currentTab.url.includes('autowikica.sharepoint.com/sites/TestPlan')) {
       link.textContent = 'New Site';
-      link.href = currentTab.url.replace('olddomain.com', 'newdomain.com');
-    } else if (currentTab.url.includes('newdomain.com')) {
+      link.href = currentTab.url.replace('autowikica.sharepoint.com/sites/TestPlan', 'autowikica.sharepoint.com/sites/IntranetSample');
+    } else if (currentTab.url.includes('autowikica.sharepoint.com/sites/IntranetSample')) {
       link.textContent = 'Old Site';
-      link.href = currentTab.url.replace('newdomain.com', 'olddomain.com');
+      link.href = currentTab.url.replace('autowikica.sharepoint.com/sites/IntranetSample', 'autowikica.sharepoint.com/sites/TestPlan');
     }
     link.addEventListener('click', function() {
       chrome.tabs.create({url: link.href});
